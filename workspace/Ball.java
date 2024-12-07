605258695d5e8bae3b9657fc4a0e19f980ce8330
diddythediddle <patiladityar27@gmail.com>
2024-12-07 02:03:31 +0000

    commit -m "submition 12/6"

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,91 @@
+//  Aditya Patil
+//  December 4, 2024
+//  General description: This class defines the Ball object used in the Brick Breaker game.
 import java.awt.*;
 
 public class Ball {
-	//your code here!
-	
-	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
-	
-	//methods:
+    private int x;
+    private int y;
+    private int size;
+    private int xVelocity;
+    private int yVelocity;
 
+    //  Constructor
+    //  Pre-condition: x, y, and size are positive integers.
+    //  Post-condition: Initializes the ball's position, size, and default velocity.
+    public Ball(int x, int y, int size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.xVelocity = 1; 
+        this.yVelocity = -2; 
+    }
+
+    //  Pre-condition: None
+    //  Post-condition: Returns the x-coordinate of the ball.
+    public int getXpos() {
+        return x;
+    }
+
+    //  Pre-condition: None
+    //  Post-condition: Returns the y-coordinate of the ball.
+    public int getYpos() {
+        return y;
+    }
+
+    //  Pre-condition: None
+    //  Post-condition: Returns the size of the ball.
+    public int getSize() {
+        return size;
+    }
+
+    //  Pre-condition: x is a valid integer.
+    //  Post-condition: Sets the x-coordinate of the ball.
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    //  Pre-condition: y is a valid integer.
+    //  Post-condition: Sets the y-coordinate of the ball.
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    //  Pre-condition: xVelocity is a valid integer.
+    //  Post-condition: Sets the ball's velocity along the x-axis.
+    public void setXVelocity(int xVelocity) {
+        this.xVelocity = xVelocity;
+    }
+
+    //  Pre-condition: yVelocity is a valid integer.
+    //  Post-condition: Sets the ball's velocity along the y-axis.
+    public void setYVelocity(int yVelocity) {
+        this.yVelocity = yVelocity;
+    }
+
+    //  Pre-condition: None
+    //  Post-condition: Reverses the ball's x-direction.
+    public void reverseX() {
+        xVelocity = -xVelocity;
+    }
+
+    //  Pre-condition: None
+    //  Post-condition: Reverses the ball's y-direction.
+    public void reverseY() {
+        yVelocity = -yVelocity;
+    }
+
+    //  Pre-condition: None
+    //  Post-condition: Updates the ball's position based on its velocity.
+    public void move() {
+        x += xVelocity;
+        y += yVelocity;
+    }
+
+    //  Pre-condition: g is a non-null Graphics object.
+    //  Post-condition: Draws the ball on the screen.
+    public void draw(Graphics g) {
+        g.setColor(Color.white);
+        g.fillOval(x, y, size, size);
+    }
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,105 @@
-import java.awt.Graphics;
+//  Aditya Patil
+//  December 5, 2024
+//  General description: This class defines the Paddle object used in the Brick Breaker game.
 import java.awt.*;
-
 public class Paddle {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int velocity;
 
-	//your code here!
-	
-	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
-	
-	//methods:
-	
-	
+    //  Constructor
+    //  Pre-condition: x, y, width, and height are positive integers.
+    //  Post-condition: Initializes the paddle's position, size, and default velocity.
+    public Paddle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.velocity = 0; 
+    }
+
+    //  Pre-condition: None
+    //  Post-condition: Returns the x-coordinate of the paddle.
+    public int getX() {
+        return x;
+    }
+
+    //  Pre-condition: None
+    //  Post-condition: Returns the y-coordinate of the paddle.
+    public int getY() {
+        return y;
+    }
+
+    //  Pre-condition: None
+    //  Post-condition: Returns the width of the paddle.
+    public int getWidth() {
+        return width;
+    }
+
+    //  Pre-condition: None
+    //  Post-condition: Returns the height of the paddle.
+    public int getHeight() {
+        return height;
+    }
+
+    //  Pre-condition: None
+    //  Post-condition: Returns the velocity of the paddle.
+    public int getVelocity() {
+        return velocity;
+    }
+
+    //  Pre-condition: x is a valid integer.
+    //  Post-condition: Sets the x-coordinate of the paddle.
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    //  Pre-condition: velocity is a valid integer.
+    //  Post-condition: Adds the given velocity to the paddle's current velocity and manges speed of the paddle. and resests if it gets to high.
+    public void addVelocity(int velocity) {
+        this.velocity += velocity;
+        if(velocity > 3){
+            velocity = 3;
+        }
+    if (velocity< -3){
+        velocity=-3;
+    }
+    }
+
+    
+
+    //  Pre-condition: velocity is a valid integer.
+    //  Post-condition: Sets the paddle's velocity.
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+
+    //  Pre-condition: None
+    //  Post-condition: Updates the paddle's position based on its velocity, ensuring it stays within bounds and  makes sure it doesnt exceed an high amount of speed..
+    public void move() {
+        x += velocity;
+        if(velocity>0){
+            velocity --;
+        }
+        if(velocity<0){
+            velocity++;
+        }
+        // Prevent paddle from moving off-screen
+        if (x < 0) {
+            x = 0;
+        }
+        if (x + width > 682) { // Assuming the screen width is 682
+        if (x + width > 682) { // screen width is 682
+            x = 682 - width;
+        }
+    }
+    }
+    //  Pre-condition: g is a non-null Graphics object.
+    //  Post-condition: Draws the paddle on the screen.
+    public void draw(Graphics g) {
+        g.setColor(Color.yellow);
+        g.fillRect(x, y, width, height);
+    }
 }
